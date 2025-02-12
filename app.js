7ed2040fb7a65cafc11d3c80dd415efea038be65
Charchit Varshney <charchit.varshney@radancy.com>
2025-02-12 14:37:11 +0530

    Episode 3 Laying the foundation

--- app.js
+++ app.js
@@ -1,73 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// <reference types="react" />
-//<reference types="react-dom" />
+//React.creatElemet() is one way to creat react element Now we will be using JSX which is Html like syntax in JS
+// File to create the Element
 
-// To create some thing like this 
-// <div id="parent">
-//     <div id="child">
-//         <h1 >
-//         Hello World from H1
-//         </h1>
-//         <h2>
-//         Hello World from H2
-//         </h2>
-//     </div>
-// </div>
-// we will be using createElement("type of element", attributes, children of the element it
-// it could be the text or the another element, In case if there are more then 1 element as children then we need to use the 
-// array to create the sibling like h1 and h2 )
 
 // const element = React.createElement("div", {id:"parent"},
-//     React.createElement("div", {id:"child"},
+//     [React.createElement("div", {id:"child"},
 //         [
-//             React.createElement("h1", {id:"heading1"}, "Hello World from H1"),
-//             React.createElement("h2", {id:"heading2"}, "Hello World from H2"),
+//             React.createElement("h1", {id:"heading1"}, "Hello World from H1 child 1"),
+//             React.createElement("h2", {id:"heading2"}, "Hello World from H2 child 1"),
 //         ]
-//      ))
+//      ),
+//     React.createElement("div", {id:"child2"},
+//         [
+//             React.createElement("h1", {id:"heading1"}, "Hello World from H1 child 2"),
+//             React.createElement("h2", {id:"heading2"}, "Hello World from H2 child 2"),
+//         ]
+//      )])
 
 // //const element = React.createElement("h1", {id:"heading"}, "Hello, React! from JS");
 
 // console.log(element);
 // const root = ReactDOM.createRoot(document.getElementById("root"))
 // root.render(element);
-//Now If we need this type of structure then
-// <div id="parent">
-//     <div id="child">
-//         <h1 >
-//         Hello World from H1
-//         </h1>
-//         <h2>
-//         Hello World from H2
-//         </h2>
-//     </div>
-//     <div id="child2">
-//         <h1 >
-//         Hello World from H1 child 2
-//         </h1>
-//         <h2>
-//         Hello World from H2 child 2
-//         </h2>
-//     </div>
-// </div>
 
-const element = React.createElement("div", {id:"parent"},
-    [React.createElement("div", {id:"child"},
-        [
-            React.createElement("h1", {id:"heading1"}, "Hello World from H1 child 1"),
-            React.createElement("h2", {id:"heading2"}, "Hello World from H2 child 1"),
-        ]
-     ),
-    React.createElement("div", {id:"child2"},
-        [
-            React.createElement("h1", {id:"heading1"}, "Hello World from H1 child 2"),
-            React.createElement("h2", {id:"heading2"}, "Hello World from H2 child 2"),
-        ]
-     )])
+const jsxHeading = <h2 id="heading">Namaste React by Charchit {2+3}</h2>;
 
-//const element = React.createElement("h1", {id:"heading"}, "Hello, React! from JS");
+const Function1=()=>{
+    return <h1>This is H1 tag</h1>;
+} 
 
-console.log(element);
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(element);
+const Function2=()=>(
+    <h1>
+        <Function1>
+        </Function1>
+        {jsxHeading}
+           This is H2 tag</h1>
+);
+
+// Function 1 and Function 2 are exactly same
+// Above line code is convereted from JSX to JS by BABEL and at the end it will be converted to REact.CreateElement code
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Function2></Function2>);
